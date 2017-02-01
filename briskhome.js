68578d4caab971c480e4ff4911b0af6e9982b3d4
Egor Zaitsev <heuels@gmail.com>
2017-02-01 09:12:05 +0300

    :wrench: [WIP] Various changes. - Changed database models for Device, Reading, Sensor. - Added database model for Event. - Minor additions to core.ldap component. - Added core.notifications component. - Added specs for core.nut component. - [WIP] Added core.pki and core.pki.forge components. - Added core.planner component. - Added core.sensors component. - Added core.users component. - Minimum Node.js version bumped to 5.0.0

--- briskhome.js
+++ briskhome.js
@@ -9,52 +9,36 @@
 
 'use strict';
 
-/* Clearing the terminal and setting the cursor to 0,0 */
-process.stdout.write('\u001b[2J\u001b[0;0H');
-console.log('[' + new Date().toISOString() + ']  INIT: briskhome/' + process.pid);
+process.stdout.write('\u001b[2J\u001b[0;0H');                 // eslint-disable-next-line no-console
+console.log(`[${new Date().toISOString()}]  INIT: briskkhome/${process.pid}`);
 
-/* Core modules. */
 const path = require('path');
-
-/* Briskhome modules. */
 const architect = require('architect');
 
-/* Other dependencies. */
 const modules = architect.loadConfig(path.join(__dirname, './lib/index.js'));
-architect.createApp(modules, function (err, app) {
+architect.createApp(modules, (err, app) => {
   if (err) {
     throw err;
   }
 
   const log = app.services.log('core');
-  log.info('Инициализация системных модулей завершена');
 
-  app.on('error', function (err) {
-    log.fatal(err);
-    setTimeout(function () {
-      process.exit(1);
-    }, 100);
+  log.info('Инициализация системных компонентов завершена');
+
+  app.on('error', (appErr) => {
+    log.fatal({ err: appErr });
+    setTimeout(process.exit(1), 100);
   });
 
-  /**
-   * Error and exception handling, pre-restart clean-up.
-   */
-  process.on('uncaughtException', function (err) {
-    log.fatal({data: err}, 'При работе приложения произошло необработанное исключение');
-    setTimeout(function () {
-      process.exit(1);
-    }, 100);
+  process.on('uncaughtException', (processErr) => {
+    console.log(processErr);
+    console.log(processErr.stack);
+    log.fatal({ err: processErr }, 'При работе приложения произошло необработанное исключение');
+    setTimeout(process.exit(1), 100);
   });
 
-  process.on('SIGINT', function () {
+  process.on('SIGINT', () => {
     log.fatal('Приложение завершило работу (SIGINT)');
-
-    // db.connection.close(function() {
-    //   // log.info('Database connection closed. Will now exit.');
-    // });
-    setTimeout(function () {
-      process.exit(0);
-    }, 100);
+    setTimeout(process.exit(0), 100);
   });
-
 });
