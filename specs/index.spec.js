5485006ddff7201ce7405c95fcc9f4e35c0f0a34
Egor Zaitsev <heuels@gmail.com>
2017-02-17 19:19:56 +0300

    Fix #27. Add specs/.eslintrc.json.

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,40 +1,48 @@
-/* global after, afterEach, before, beforeEach, describe, it */
-/* eslint import/no-dynamic-require: [0]                                                          */
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-/* eslint global-require: [0] */
-/* eslint func-names: [0] */
-/* eslint prefer-arrow-callback: [0] */
-
 /**
- * @briskhome </specs/briskhome.spec.js>
- *
- * Test suites for BRISKHOME.
- *
- * @author  Egor Zaitsev <ezaitsev@briskhome.com>
+ * @briskhome/core
+ * └<specs/index.spec.js>
+ * @author Egor Zaitsev <ezaitsev@briskhome.com>
  */
 
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
+const assert = require('chai').assert;
+
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.statSync(path.join(srcpath, file)).isDirectory());
+}
 
 describe('BRISKHOME', function () {
-  it('should prepare a list of installed components');
+  const components = [];
+  const specs = [];
 
-  function getDirectories(srcpath) {
-    return fs.readdirSync(srcpath)
-    .filter(file => fs.statSync(path.join(srcpath, file)).isDirectory());
-  }
-
-  const components = getDirectories('./lib');
-  for (let i = 0; i < components.length; i += 1) {
-    if (components[i].indexOf('core.') === 0) {
-      const subdirectories = getDirectories(path.resolve('./lib', components[i]));
-      if (subdirectories.indexOf('specs') > -1) {
-        describe(components[i], function () {
-          require(path.resolve('./lib', components[i], 'specs', 'index.spec.js'));
-        });
-      }
+  it('should load a list of core components', function () {
+    const directories = getDirectories('./lib');
+    for (let i = 0; i < directories.length; i += 1) {
+      if (directories[i].indexOf('core.') === 0) components.push(directories[i]);
     }
-  }
+
+    assert.isNotNull(components);
+  });
+
+  it('should load specs from each core component', function () {
+    for (let i = 0; i < components.length; i += 1) {
+      const directories = getDirectories(path.resolve('./lib', components[i]));
+      if (directories.indexOf('specs') > -1) specs.push(components[i]);
+    }
+
+    // FIXME: The next line should be uncommented after #29 is marked as resolved.
+    // assert.equal(specs.length, components.length, 'not every core component has specs');
+  });
+
+  after(function () {
+    for (let i = 0; i < specs.length; i += 1) {
+      describe(specs[i], function () {
+        require(path.resolve('./lib', specs[i], 'specs', 'index.spec.js'));
+      });
+    }
+  });
 });
