e1677ee5c96069ccc2665a65cd2be5d45d998c81
Egor Zaytsev <ezaitsev@briskhome.com>
2017-04-29 21:08:39 +0300

    Add enable/disable component feature

--- src/components.js
+++ src/components.js
@@ -8,47 +8,67 @@ import path from 'path';
 
 import type { PackageJson } from './utilities/coreTypes';
 
-const components = (directories?: Array<string> = ['./lib', './node_modules']): Array<string> => {
+export const components = (directories?: Array<string> = ['./lib', './node_modules'])
+  : Array<string> => {
   let modules = [];
-  for (let i = 0; i < directories.length; i += 1) { // directories.map()?
+  for (let i = 0; i < directories.length; i += 1) {
     const directory = directories[i];
     modules = modules.concat(fs.readdirSync(directory)
       .filter(subdirectory => (directory === './lib'
-        ? fs.statSync(path.resolve(directory, subdirectory)).isDirectory() && subdirectory.indexOf('core.') === 0
-        : fs.statSync(path.resolve(directory, subdirectory)).isDirectory() && subdirectory.indexOf('briskhome-') === 0))
+        ? fs.statSync(path.resolve(directory, subdirectory)).isDirectory() &&
+          subdirectory.indexOf('core.') === 0
+        : fs.statSync(path.resolve(directory, subdirectory)).isDirectory() &&
+          subdirectory.indexOf('briskhome-') === 0))
       .map(subdirectory => path.resolve(directory, subdirectory)));
   }
 
   return modules;
 };
 
-const inspectComponent = (directory: string): PackageJson =>
-  JSON.parse(String(fs.readFileSync(path.resolve(directory, 'package.json'))),
-);
+export const inspectComponent = (directory: string)
+  : PackageJson =>
+  JSON.parse(String(fs.readFileSync(path.resolve(directory, 'package.json'))));
 
-const enabledComponents = (directories?: Array<string>): Array<string> => components(directories)
+export const enabledComponents = (directories?: Array<string>)
+  : Array<string> => components(directories)
   .filter(directory =>
-    inspectComponent(directory).plugin && !inspectComponent(directory).plugin.disabled,
-);
+    inspectComponent(directory).plugin && !inspectComponent(directory).plugin.disabled);
 
-const disabledComponents = (directories?: Array<string>): Array<string> => components(directories)
+export const disabledComponents = (directories?: Array<string>)
+  : Array<string> => components(directories)
   .filter(directory =>
-    inspectComponent(directory).plugin && !inspectComponent(directory).plugin.disabled,
-);
+    inspectComponent(directory).plugin && inspectComponent(directory).plugin.disabled);
 
-const requireResources = (directory: string): Array<string> => (
+export const requireResources = (directory: string)
+  : Array<string> => (
   [].concat(...enabledComponents()
     .map(component => (fs.readdirSync(component).includes(directory)
       ? fs.readdirSync(path.resolve(component, directory))
         .map(resource => path.resolve(component, directory, resource))
       : []))
-    .filter(resource => !!resource))
-);
+    .filter(resource => !!resource)));
 
-module.exports = {
-  components,
-  inspectComponent,
-  enabledComponents,
-  disabledComponents,
-  requireResources,
-};
+export const enableComponent = (directory: string)
+  : boolean =>
+  !!disabledComponents().filter((component) => {
+    if (component.includes(directory)) {
+      const payload = JSON.parse(String(fs.readFileSync(path.resolve(component, 'package.json'))));
+      payload.plugin.disabled = false;
+      fs.writeFileSync(path.resolve(component, 'package.json'), JSON.stringify(payload, null, 2));
+      return true;
+    }
+    return false;
+  }).length;
+
+export const disableComponent = (directory: string)
+  : boolean =>
+  !!enabledComponents().filter((component) => {
+    if (component.includes(directory)) {
+      const payload = JSON.parse(String(fs.readFileSync(path.resolve(component, 'package.json'))));
+      payload.plugin.disabled = true;
+      fs.writeFileSync(path.resolve(component, 'package.json'), JSON.stringify(payload, null, 2));
+      return true;
+    }
+    return false;
+  }).length;
+
