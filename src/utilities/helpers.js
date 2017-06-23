238e75cb3fe3bb44b2804bb93358878af444f67e
Egor Zaitsev <heuels@gmail.com>
2017-06-23 10:30:55 +0300

    Add support for parsing package.json to 'getCallee()' (#64)

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -2,9 +2,18 @@
  * @briskhome
  * └core <utilities/helpers.js>
  */
+import path from 'path';
 
-export const getCallee = (depth?: number = 3): string =>
-  new Error().stack.split('\n')[depth].split('/').reduce((acc, s, i, arr) =>
-    (s === 'lib' || s === 'node_modules' ? arr[i + 1] : acc), '').split(':').shift();
-
-export const a = null;
+/**
+ * Determines either the package name or the directory of the module that issued a call.
+ * @param {Number} depth  A level of stack trace that will be used for determining the callee.
+ * @returns {String} Either a name of directory or module name from package.json.
+ */
+export const getCallee = (depth?: number = 3): string => {
+  const d = new Error().stack.split('\n')[depth].split('/').slice(1, -1);
+  try {
+    return require(path.join('/', d.join('/'), 'package.json')).name;                             // eslint-disable-line
+  } catch (e) {
+    return d.reduce((acc, s, i, a) => (s === 'lib' || s === 'node_modules' ? a[i + 1] : acc), '').split(':').shift();
+  }
+};
