0ee3c8a5f87f941dc7884670d6210be19eed4e51
Egor Zaitsev <heuels@gmail.com>
2017-10-04 14:29:05 +0200

    Export objects instead of functions from GraphQL types (#92)

--- src/core.graphql/types/Device.js
+++ src/core.graphql/types/Device.js
@@ -3,39 +3,31 @@
  * └core.graphql <types/Device.js>
  */
 
-import {
-  GraphQLString,
-  GraphQLObjectType,
-  GraphQLList,
-} from 'graphql';
+import { GraphQLString, GraphQLObjectType, GraphQLList } from 'graphql';
 import SensorType from './Sensor';
-import type { CoreImports } from '../../utilities/coreTypes';
 
-export default (imports: CoreImports): GraphQLObjectType => {
-  const { dataloader, db } = imports;
-  const SensorModel = db.model('core:sensor');
-  const Sensor = SensorType(imports);
-  return new GraphQLObjectType({
-    name: 'Device',
-    description: 'This is a generic device',
-    fields: {
-      id: {
-        type: GraphQLString,
-        description: 'Unique device identifier',
-      },
-      name: {
-        type: GraphQLString,
-        description: 'Human-readable device name',
-      },
-      sensors: {
-        type: new GraphQLList(Sensor),
-        description: 'Sensors that are connected to or operated by this device',
-        resolve: async (src) => {
-          const ids = await SensorModel.sensorsByDeviceId(src.id);
-          if (!ids.length) return null;
-          return dataloader.sensorById.loadMany(ids.reduce((acc, doc) => acc.concat(doc._id), []));
-        },
+export default new GraphQLObjectType({
+  name: 'Device',
+  description: 'This is a generic device',
+  fields: {
+    id: {
+      type: GraphQLString,
+      description: 'Unique device identifier',
+    },
+    name: {
+      type: GraphQLString,
+      description: 'Human-readable device name',
+    },
+    sensors: {
+      type: new GraphQLList(SensorType),
+      description: 'Sensors that are connected to or operated by this device',
+      resolve: async (src, args, ctx) => {
+        const { db, dataloader } = ctx;
+        const SensorModel = db.model('core:sensor');
+        const ids = await SensorModel.sensorsByDeviceId(src.id);
+        if (!ids.length) return null;
+        return dataloader.sensorById.loadMany(ids.reduce((acc, doc) => acc.concat(doc._id), []));
       },
     },
-  });
-};
+  },
+});
