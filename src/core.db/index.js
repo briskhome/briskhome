bc5d5aaa9360f551c357633db80bf439f60dd5a2
Egor Zaitsev <ezaitsev@briskhome.com>
2017-12-15 11:00:06 +0300

    refactor(core.db): accept connection string instead of constructing it

--- src/core.db/index.js
+++ src/core.db/index.js
@@ -16,23 +16,19 @@ export default (
   imports: CoreImports,
   register: CoreRegister,
 ) => {
-  const { database, hostname, username, password } = options;
+  const { uri } = options;
   const bus = imports.bus;
   const log = imports.log();
 
   let ready = false;
   bus.on('core:ready', () => (ready = true));
 
-  let credentials = '';
-  if (username)
-    credentials = password ? `${username}:${password}@` : `${username}@`;
-
   mongoose.Promise = global.Promise;
   mongoose.set('debug', options.NODE_ENV !== 'production');
-  mongoose.connect(`mongodb://${credentials}${hostname}/${database}`);
+  mongoose.connect(uri);
   mongoose.connection.on('error', err => {
     log.fatal(
-      { err, hostname, database, username },
+      { err, uri },
       'Error establishing connection to MongoDB instance',
     );
     if (ready) bus.emit('core:error', err);
@@ -40,39 +36,24 @@ export default (
   });
 
   mongoose.connection.on('connecting', () => {
-    log.debug(
-      { hostname, database, username, password },
-      'Trying to connect to MongoDB instance',
-    );
+    log.debug({ uri }, 'Trying to connect to MongoDB instance');
   });
 
   mongoose.connection.once('connected', () => {
-    log.info(
-      { hostname, database, username },
-      'Database connection established',
-    );
+    log.info({ uri }, 'Database connection established');
     resources('models', [{ ...imports, db: mongoose }]);
     return register(null, { db: mongoose });
   });
 
   mongoose.connection.on('disconnecting', () => {
-    log.debug(
-      { hostname, database, username },
-      'Disconnecting from MongoDB instance',
-    );
+    log.debug({ uri }, 'Disconnecting from MongoDB instance');
   });
 
   mongoose.connection.on('disconnected', () => {
-    log.warn(
-      { hostname, database, username },
-      'Disconnected from MongoDB instance',
-    );
+    log.warn({ uri }, 'Disconnected from MongoDB instance');
   });
 
   mongoose.connection.on('reconnected', () => {
-    log.info(
-      { hostname, database, username },
-      'Reconnected to MongoDB instance',
-    );
+    log.info({ uri }, 'Reconnected to MongoDB instance');
   });
 };
