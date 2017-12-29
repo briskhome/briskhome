8562bde75d781583148a4857a123f7a2532f7b51
Egor Zaitsev <ezaitsev@briskhome.com>
2017-12-29 10:57:46 +0300

    feat(core.webapp): self-hosting roboto typeface

--- src/core.webapp/app/index.jsx
+++ src/core.webapp/app/index.jsx
@@ -2,4 +2,6 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Routes } from './routes';
 
+import 'typeface-roboto';
+
 ReactDOM.render(<Routes />, document.getElementById('briskhome'));
