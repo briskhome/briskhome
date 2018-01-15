17b6b6dc3a687500297b014b9ab327ca16b31efa
Egor Zaitsev <ezaitsev@briskhome.com>
2018-01-16 00:55:18 +0300

    feat(core.webapp): icon component

---
+++ src/core.webapp/components/ui/icon/index.jsx
@@ -0,0 +1,16 @@
+/** @flow */
+import * as React from 'react';
+import cn from 'classnames';
+import './index.styl';
+
+type IconProps = { className?: string, name: string };
+
+const Icon = ({ className, name }: IconProps): React.Node => {
+  return (
+    <span
+      className={cn('briskhome-icon', `briskhome-icon__${name}`, className)}
+    />
+  );
+};
+
+export default Icon;
