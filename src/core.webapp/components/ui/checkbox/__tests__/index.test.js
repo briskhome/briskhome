f8b0821859681de47444c0752db2ca1f79d49c16
Egor Zaitsev <ezaitsev@briskhome.com>
2018-01-16 00:35:21 +0300

    feat(core.webapp): checkbox component

---
+++ src/core.webapp/components/ui/checkbox/__tests__/index.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { render } from 'enzyme';
+
+import { Checkbox } from '../../';
+
+describe('core.webapp -> ui -> checkbox', () => {
+  it('renders', () => {
+    expect(
+      toJson(
+        render(
+          <Checkbox name="example" checked={false} onChange={() => null} />,
+        ),
+      ),
+    ).toMatchSnapshot();
+  });
+  it('renders checked', () => {
+    expect(
+      toJson(
+        render(
+          <Checkbox name="example" checked={true} onChange={() => null} />,
+        ),
+      ),
+    ).toMatchSnapshot();
+  });
+  it('renders disabled', () => {
+    expect(
+      toJson(
+        render(<Checkbox name="example" checked={true} disabled={true} />),
+      ),
+    ).toMatchSnapshot();
+  });
+  it('renders with className', () => {
+    expect(
+      toJson(render(<Checkbox checked={false} className="example" />)),
+    ).toMatchSnapshot();
+  });
+});
