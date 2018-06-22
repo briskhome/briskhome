b4abbbb057abf0fbf62d4e1bcfa6688decfeb875
Egor Zaitsev <zaytsevev@profi.ru>
2018-06-22 20:54:39 +0300

    refactor(log): update module to use extensions api

--- src/core.log/index.js
+++ src/core.log/index.js
@@ -5,34 +5,24 @@
 
 import bunyan from 'bunyan';
 import { getCallee, normalizeName } from '../utilities/helpers';
-import type {
-  CoreOptions,
-  CoreImports,
-  CoreRegister,
-} from '../utilities/coreTypes';
+import type { CoreOptions, CoreImports } from '../utilities/coreTypes';
 
-export default (
-  options: CoreOptions,
-  imports: CoreImports,
-  register: CoreRegister,
-) => {
+export default (options: CoreOptions, imports: CoreImports) => {
   const log = bunyan.createLogger({
     name: 'briskhome',
     streams: [
       {
-        level: options.level,
+        level: options.level || 10,
         stream: process.stdout,
       },
     ],
   });
 
-  register(null, {
-    log: (name?: string) => {
-      const child = log.child({
-        component: name || normalizeName(getCallee()),
-      });
-      child.debug('Initialized logger instance for component');
-      return child;
-    },
-  });
+  return (name?: string) => {
+    const child = log.child({
+      component: name || normalizeName(getCallee()),
+    });
+    child.debug('Initialized logger instance for component');
+    return child;
+  };
 };
